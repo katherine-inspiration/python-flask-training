bd92b5efe2e8b81a35e82d3de39c778c542e7260
Katherine <katherine.inspiration@gmail.com>
2021-08-03 10:31:40 +0100

    Added post and put requests api for persons

--- middleware.py
+++ middleware.py
@@ -1,3 +1,43 @@
+from flask import jsonify, request, abort
+
+
+people_info = [
+    {
+        'name': "Jack The Sparrow",
+        'age': 40,
+        'occupation': 'pirate'
+    },
+    {
+        'name': "Freddie Mercury",
+        'age': 38,
+        'occupation': 'rockstar'
+    },
+    {
+        'name': "Katty Perry",
+        'age': 30,
+        'occupation': 'singer'
+    },
+]
+
+def persons():
+    return jsonify(people_info)
+
+
+def person_add():
+    try:
+        data = request.get_json(force=True)
+        name = data["name"]
+        age = data["age"]
+        occupation = data["occupation"]
+        if name and age and occupation:
+            people_info.append({"name": name, "age": age, "occupation": occupation})
+            return jsonify({"Person " + name: "Added successfully"})
+    except Exception as exc:
+        print(exc)
+        abort(400)
+
+
+
 def hello():
     return "Hello world from Flask :)"
 
@@ -8,3 +48,29 @@ def hello_from():
 
 def db():
     return "Hello DB Grads!"
+
+
+def person_update():
+
+    try:
+        data = request.get_json(force=True)
+        name = data["name"]
+        age = data["age"]
+        occupation = data["occupation"]
+        if age and name and occupation:
+            for i in range(len(people_info)):
+                if people_info[i]["name"] == name:
+                    people_info[i]["age"] = age
+                    people_info[i]["occupation"] = occupation
+                    return jsonify({"Person " + name: "Updated"})
+
+            abort(400)
+        else:
+            abort(400)
+    except Exception as exc:
+        print(exc)
+        abort(400)
+
+
+
+
--- routes.py
+++ routes.py
@@ -7,9 +7,18 @@ def initialize_routes(app):
     app.add_url_rule("/api/db/", "db", db)
     app.add_url_rule("/api/hello_from/", "hello_from", hello_from)
     app.add_url_rule("/api/", "list_routes", list_routes, defaults={'app': app})
+    app.add_url_rule("/api/persons/", "persons", persons)
+    app.add_url_rule("/api/persons/", "person_add", person_add, methods=["POST"])
+    app.add_url_rule("/api/persons/", "person_update", person_update, methods=["PUT"])
 
 
 def list_routes(app):
+    route_list = {}
     for route in app.url_map.iter_rules():
-        print(route.methods, route.endpoint, '-', route)
-    return "Routes Info Goes Here"
+        route_dict = {
+            "Route": str(route),
+            "Endpoint": route.endpoint,
+            "Methods": list(route.methods)
+        }
+        route_list[route.endpoint] = route_dict
+    return jsonify({"Routes": route_list, "Total": len(route_list)})
